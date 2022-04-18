705efe7e692a3b02ebb9173b6577bb7486abac62
aleksandra.mitro <sandramitro99@gmail.com>
2022-04-18 18:56:25 +0200

    Add project files.

---
+++ Dislinkt.Profile.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislinkt.Profile.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Dislinkt.Profile/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dislinkt.Profile.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        /// <summary>
+        /// Register new user
+        /// </summary>
+        /// <returns>A boolean status of registration</returns>
+        [HttpPost]
+        public bool RegisterUser()
+        {
+            return false;
+        }
+    }
+}
