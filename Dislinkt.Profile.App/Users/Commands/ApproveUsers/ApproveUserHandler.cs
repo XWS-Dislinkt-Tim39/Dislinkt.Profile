25af558a4dc59f6ee0cfd03569301f9757aa2a34
aleksandra.mitro <sandramitro99@gmail.com>
2022-04-24 13:07:12 +0200

    done user approval

---
+++ Dislinkt.Profile.App/Users/Commands/ApproveUsers/ApproveUserHandler.cs
@@ -0,0 +1,26 @@
+﻿using Dislinkt.Profile.Core.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dislinkt.Profile.App.Users.Commands.ApproveUsers
+{
+    public class ApproveUserHandler : IRequestHandler<ApproveUserCommand, bool>
+    {
+        private readonly IUserRepository _userRepository;
+        public ApproveUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<bool> Handle(ApproveUserCommand request, CancellationToken cancellationToken)
+        {
+            var existingUser = await _userRepository.GetByIdAsync(request.Request);
+
+            if (existingUser == null) return false;
+
+            await _userRepository.ApproveUserAsync(request.Request);
+
+            return true;
+        }
+    }
+}
