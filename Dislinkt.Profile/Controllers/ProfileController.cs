37eed113d26f1a4da19cfb456f349178b7f04480
KNatali <natatsi24@gmail.com>
2022-05-31 11:48:05 +0200

    change controller description for sign in

--- Dislinkt.Profile/Controllers/ProfileController.cs
+++ Dislinkt.Profile/Controllers/ProfileController.cs
@@ -223,14 +223,14 @@ namespace Dislinkt.Profile.WebApi.Controllers
         /// Sign up user
         /// </summary>
         /// <returns>Registred user or null if user dont exist</returns>
-        /// /// <param name="emailAddress">for user</param>
+        /// /// <param name="username">for user</param>
         /// /// <param name="password">for user</param>
         [HttpGet]
         [SwaggerOperation(Tags = new[] { ApiTag })]
         [Route("/sign-up")]
-        public async Task<UserDetails> SignUpUserAsync(string emailAddress, string password)
+        public async Task<UserDetails> SignUpUserAsync(string username, string password)
         {
-            return await _mediator.Send(new SignUpCommand(emailAddress, password));
+            return await _mediator.Send(new SignUpCommand(username, password));
         }
         /// <summary>
         /// Get all users
